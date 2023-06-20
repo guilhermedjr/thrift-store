2ad3ebd6c85951c40bf28eeaebd791620062a13b
Guilherme Djrdjrjan Santos <guilhermedjrdjrjan@gmail.com>
2023-06-20 11:39:38 -0300

    ✅ Add Product tests

--- thrift-store-api/src/thrift_store/entities/product.clj
+++ thrift-store-api/src/thrift_store/entities/product.clj
@@ -10,55 +10,64 @@
           price :- Double
           stock :- s/Int])
 
-;;(s/defrecord
- ;;ProductStore [product :- Product
-               ;;store :- Store
-               ;;name :- s/Str
-               ;;description :- s/Str
-               ;;price :- Double
-               ;;stock :- s/Int])
-
-;; todo: unit tests
-;;(with-test "Stock update"
-  ;;(s/defn dec-stock
-    ;;([p :- Product] (update p :stock dec))
-    ;;([p :- Product qty :- s/Int] (update p :stock - qty)))
-  ;;(testing "by simple increment"
-    ;;(is (= 9 (dec-stock ))))
-  ;;(testing "by specified increment"))(
-
-(with-test (s/defn dec-stock
-             ([p :- Product] (update p :stock dec))
-             ([p :- Product qty :- s/Int] (update p :stock - qty))))
-
-(s/defn inc-stock 
-  ([p :- Product] (update p :stock inc)) 
-  ([p :- Product qty :- s/Int] (update p :stock + qty)))
-
-(s/defn change-price
-  [p :- Product
-   price :- Double]
-    (update p :price price))
-
-(s/defn apply-discount
-  [p :- Product
-   discount :- Float]
-    (update p :price - (* (/ (get p :price) 100) discount)))
-
-(s/defn change-description
-  [p :- Product
-   d :- s/Str]
-    (update p :description d))
-
-(s/defn change-name
-  [p :- Product
-   name :- s/Str]
-    (update p :name name))
-
-;;(s/defn bind-product-to-store
-  ;;([product stores] ())
-  ;;([product store] ()))
-
 (s/defn ^:private product
   ([name price stock] (product name nil price stock))
   ([name description price stock] (->Product (str (java.util.UUID/randomUUID)) name description price stock)))
+
+(def test-product 
+  (product "TV LG" "TV LG" 1999.00 5))
+
+(with-test
+  (s/defn dec-stock
+    ([p :- Product] (update p :stock dec))
+    ([p :- Product qty :- s/Int] (update p :stock - qty)))
+  (testing "Testing 'dec-stock'" 
+    (let [product test-product]
+      (is (= 4 (:stock (dec-stock product))))
+      (is (= 2 (:stock (dec-stock product 3)))))))
+
+(with-test
+  (s/defn inc-stock
+    ([p :- Product] (update p :stock inc))
+    ([p :- Product qty :- s/Int] (update p :stock + qty)))
+  (testing "Testing 'inc-stock'"
+     (let [product test-product]
+       (is (= 6 (:stock (inc-stock product))))
+       (is (= 8 (:stock (inc-stock product 3)))))))
+
+(with-test
+  (s/defn change-price
+    [p :- Product
+     price :- Double]
+    (assoc p :price price))
+  (testing "Testing 'change-price'"
+    (let [product test-product]
+      (is (= 1599.00 (:price (change-price product 1599.00)))))))
+
+(with-test
+  (s/defn apply-discount
+    [p :- Product
+     discount :- Float]
+    (update p :price - (* (/ (get p :price) 100) discount)))
+  (testing "Testing 'apply-discount'"
+    (let [product test-product]
+      (is (= 1199.40 (:price (apply-discount product 40)))))))
+
+(with-test
+  (s/defn change-description
+    [p :- Product
+     d :- s/Str]
+    (assoc p :description d))
+  (testing "Testing 'change-description'"
+   (let [product test-product]
+     (is (= "TV LG de ótima qualidade"
+            (:description (change-description product "TV LG de ótima qualidade")))))))
+
+(with-test
+  (s/defn change-name
+    [p :- Product
+     name :- s/Str]
+    (assoc p :name name))
+  (testing "Testing 'change-name'"
+   (let [product test-product]
+     (is (= "TV LG 42" (:name (change-name product "TV LG 42")))))))
